1e2b0cbcc7cfe710cd4ad6701050bae1712e9a69
Yazid <Alabaster456@users.noreply.github.com>
2014-03-16 18:19:38 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,17 @@
-$(document).ready();
+$(document).ready(function(){
+  $('#test').fadeIn('slow'); {
+$('#test').fadeOut('slow');
+  } 
+  $('#test1').delay(1500).fadeIn('slow');{
+$('#test1').fadeOut('slow'); {
+    }
+  } $('#test2').delay(3000).fadeIn('slow'); {
+ $('#test2').fadeOut('slow'); {
+      
+    
+  }
+  }
+  $('#background').delay(5000).fadeIn('slow'); {
+   $('#sonice').delay(6000).fadeIn('slow'); 
+  }
+});
