a9658633e6bc55f631b080bf12b5231631654b7b
Yazid <Alabaster456@users.noreply.github.com>
2014-03-18 15:41:23 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -12,6 +12,59 @@ $('#test1').fadeOut('slow'); {
   }
   }
   $('#background').delay(5000).fadeIn('slow'); {
-   $('#sonice').delay(6000).fadeIn('slow'); 
+    $('#check').delay(6500).fadeIn('slow');  
   }
-});
+$('#sonicrun').delay(7000).fadeIn('slow'); {
+  watch();
+  watch();
+}
+ $('#sonicrun').fadeOut('fast'); $('#sonicdash').delay(11900).fadeIn('fast'); { 
+watch2();
+  }
+  $('#title').delay(12000).fadeIn('slow'); {
+$('#play').delay(12100).fadeIn('slow');
+  $('#info').delay(12100).fadeIn('slow');
+  $('#About').delay(12100).fadeIn('slow');  }
+  });
+
+//This shortens the JQuery code.
+function watch(sonic) {
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+  $('#sonicrun').animate({left: '+=10px'}, 100);
+   $('#sonicrun').animate({left: '+=10px'}, 100);
+ 
+}
+//Animation when sonic runs.
+function watch2 () { 
+  $('#sonicdash').animate({left: '+=10px'}, 100);
+   $('#sonicdash').animate({left: '+=10px'}, 100);
+  $('#sonicdash').animate({left: '+=10px'}, 100);
+   $('#sonicdash').animate({left: '+=10px'}, 100);
+  $('#sonicdash').animate({left: '+=10px'}, 100);
+   $('#sonicdash').animate({left: '+=10px'}, 100);
+   $('#sonicdash').animate({left: '+=60px'}, 100);
+    $('#sonicdash').animate({left: '+=60px'}, 100);
+   $('#sonicdash').animate({left: '+=60px'}, 100);
+    $('#sonicdash').animate({left: '+=60px'}, 100);
+   $('#sonicdash').animate({left: '+=60px'}, 100);
+    $('#sonicdash').animate({left: '+=60px'}, 100);
+   $('#sonicdash').animate({left: '+=60px'}, 100);
+ $('#sonicdash').animate({left: '+=60px'}, 100); $('#sonicdash').delay(300).fadeOut('fast');
+}
